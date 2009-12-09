8247b70914bccd532eaf49155a53f2bb5e09a54f
Aecio Santos <aecio.solando@gmail.com>
2009-12-09 03:53:19 -0300

    Alterações no AlunoController.java, AlunoControllerImpl.java e AlunoView.java.

--- src/org/fpij/jitakyoei/controller/AlunoController.java
+++ src/org/fpij/jitakyoei/controller/AlunoController.java
@@ -4,6 +4,6 @@ import org.fpij.jitakyoei.model.beans.Aluno;
 
 public interface AlunoController {
 	public void handleCreateAction(Aluno obj);
-	public void handleDeleteAction(Aluno obj);
 	public void handleUpdateAction(Aluno obj);
+	public void handleSearchAction(Aluno obj);
 }
--- src/org/fpij/jitakyoei/controller/AlunoControllerImpl.java
+++ src/org/fpij/jitakyoei/controller/AlunoControllerImpl.java
@@ -1,24 +1,51 @@
 package org.fpij.jitakyoei.controller;
 
+import java.util.List;
+
+import org.fpij.jitakyoei.model.AlunoModel;
 import org.fpij.jitakyoei.model.beans.Aluno;
+import org.fpij.jitakyoei.view.AlunoView;
 
 public class AlunoControllerImpl implements AlunoController{
-
-	@Override
-	public void handleCreateAction(Aluno obj) {
-		// TODO Auto-generated method stub
-		
+	AlunoModel model;
+	AlunoView view;
+	
+	public AlunoControllerImpl(AlunoModel model, AlunoView view) {
+		this.model = model;
+		this.view = view;
 	}
 
+	private void reportException(Exception e) {
+		view.displayException(e.getMessage());
+	}
+	
 	@Override
-	public void handleDeleteAction(Aluno obj) {
-		// TODO Auto-generated method stub
-		
+	public void handleCreateAction(Aluno obj) {
+		try{
+			obj = model.save(obj);
+			view.displayResult(obj);
+		}catch (Exception e) {
+			reportException(e);
+		}
 	}
 
 	@Override
 	public void handleUpdateAction(Aluno obj) {
-		// TODO Auto-generated method stub
-		
+		try{
+			obj = model.update(obj);
+			view.displayResult(obj);
+		}catch (Exception e) {
+			reportException(e);
+		}
+	}
+
+	@Override
+	public void handleSearchAction(Aluno obj) {
+		try{
+			List<Aluno> result = model.find(obj);
+			view.displayResult(result);
+		}catch (Exception e) {
+			reportException(e);
+		}
 	}
 }
