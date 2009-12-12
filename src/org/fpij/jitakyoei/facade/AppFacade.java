4d5dd4023e9bd2602b806724bdd9ff89522bd042
Aecio Santos <aecio.solando@gmail.com>
2009-12-12 15:47:05 -0300

    Adição do AppFacade.java e AppFacadeImpl.java

---
+++ src/org/fpij/jitakyoei/facade/AppFacade.java
@@ -0,0 +1,13 @@
+package org.fpij.jitakyoei.facade;
+
+public interface AppFacade {
+
+	public abstract void createAluno();
+
+	public abstract void updateAluno();
+
+	public abstract void searchAluno();
+
+	public abstract void listAlunos();
+
+}
---
+++ src/org/fpij/jitakyoei/facade/AppFacadeImpl.java
@@ -0,0 +1,20 @@
+package org.fpij.jitakyoei.facade;
+
+public class AppFacadeImpl implements AppFacade {
+	
+	public void createAluno() {
+	}
+
+	public void updateAluno() {
+
+	}
+
+	public void searchAluno() {
+
+	}
+
+	public void listAlunos() {
+
+	}
+
+}
