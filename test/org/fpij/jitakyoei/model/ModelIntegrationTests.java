54b6ad9c958556a8f1bf2936e663aecbd2a3862d
Aecio Santos <aecio.solando@gmail.com>
2009-12-12 19:46:01 -0300

    Adição de classe de teste ModelIntegrationTests.java.

---
+++ test/org/fpij/jitakyoei/model/ModelIntegrationTests.java
@@ -0,0 +1,61 @@
+package org.fpij.jitakyoei.model;
+
+import java.util.Date;
+
+import org.fpij.jitakyoei.model.beans.*;
+import org.fpij.jitakyoei.model.dao.*;
+import org.fpij.jitakyoei.util.DatabaseManager;
+import org.junit.BeforeClass;
+
+public class ModelIntegrationTests {
+	private static DAO<Aluno> alunoDao;
+	private static Aluno aluno;
+	private static Entidade entidade;
+	private static Endereco endereco;
+	private static Filiado filiadoAluno;
+	private static Filiado filiadoProf;
+	private static Professor professor;
+	
+	@BeforeClass
+	public static void setUp(){
+		DatabaseManager.setEnviroment(DatabaseManager.TEST);
+		filiadoAluno = new Filiado();
+		filiadoAluno.setNome("Aécio");
+		filiadoAluno.setCpf("036.464.453-27");
+		filiadoAluno.setDataNascimento(new Date());
+		filiadoAluno.setDataCadastro(new Date());
+		filiadoAluno.setId(1332L);
+		filiadoAluno.setRg(new Rg("5026762-0", "SSP-PI"));
+		
+		endereco = new Endereco();
+		endereco.setBairro("Dirceu");
+		endereco.setCep("64078-213");
+		endereco.setCidade("Teresina");
+		endereco.setEstado("PI");
+		endereco.setRua("Rua Des. Berilo Mota");
+		
+		filiadoProf = new Filiado();
+		filiadoProf.setNome("Professor");
+		filiadoProf.setCpf("036.464.453-27");
+		filiadoProf.setDataNascimento(new Date());
+		filiadoProf.setDataCadastro(new Date());
+		filiadoProf.setId(3332L);
+		filiadoProf.setEndereco(endereco);
+		
+		professor = new Professor();
+		professor.setFiliado(filiadoProf);
+		
+		entidade = new Entidade();
+		entidade.setEndereco(endereco);
+		entidade.setNome("Academia 1");
+		entidade.setTelefone1("(086)1234-5432");
+		
+		aluno = new Aluno();
+		aluno.setFiliado(filiadoAluno);
+		aluno.setProfessor(professor);
+		aluno.setEntidade(entidade);
+		
+		alunoDao = new DAOImpl<Aluno>(Aluno.class);
+	}
+	
+}
