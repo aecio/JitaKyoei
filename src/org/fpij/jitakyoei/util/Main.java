3d0cefbe053ad26852e535f254ed88741afbd89d
Aecio Santos <aecio.solando@gmail.com>
2009-12-12 20:16:32 -0300

    Adicao o Nimbus Look and Feel

--- src/org/fpij/jitakyoei/util/Main.java
+++ src/org/fpij/jitakyoei/util/Main.java
@@ -1,12 +1,24 @@
 package org.fpij.jitakyoei.util;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 import org.fpij.jitakyoei.view.MainAppView;
-import com.pagosoft.plaf.PlafOptions;
 
 public class Main {
 	
 	public static void main(String[] args) {
 //		PlafOptions.setAsLookAndFeel();
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+		    // If Nimbus is not available, you can set the GUI to another look and feel.
+		}
 		new MainAppView();
 	}
 }
