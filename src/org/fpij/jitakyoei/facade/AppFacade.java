bcb54369a7803afbec3192a9c5faaf91c0aa5fc6
Aecio Santos <aecio.solando@gmail.com>
2009-12-13 06:25:06 -0300

    Implementação de métodos interface AppFacade na classe AppFacadeImpl para os beans Professores e Entidades.

--- src/org/fpij/jitakyoei/facade/AppFacade.java
+++ src/org/fpij/jitakyoei/facade/AppFacade.java
@@ -1,15 +1,23 @@
 package org.fpij.jitakyoei.facade;
 
 import org.fpij.jitakyoei.model.beans.Aluno;
+import org.fpij.jitakyoei.model.beans.Entidade;
+import org.fpij.jitakyoei.model.beans.Professor;
 
 public interface AppFacade {
 
 	public abstract void createAluno(Aluno aluno);
-
 	public abstract void updateAluno(Aluno aluno);
-
 	public abstract void searchAluno(Aluno aluno);
+	public abstract void listAlunos();
+	
+	public abstract void createProfessor(Professor professor);
+	public abstract void updateProfessor(Professor professor);
+	public abstract void searchProfessor(Professor professor);
+	public abstract void listProfessores();
 
-	public abstract void listAlunos(Aluno aluno);
-
+	public abstract void createEntidade(Entidade entidade);
+	public abstract void updateEntidade(Entidade entidade);
+	public abstract void searchEntidade(Entidade entidade);
+	public abstract void listEntidade();
 }
--- src/org/fpij/jitakyoei/facade/AppFacadeImpl.java
+++ src/org/fpij/jitakyoei/facade/AppFacadeImpl.java
@@ -2,19 +2,29 @@ package org.fpij.jitakyoei.facade;
 
 import javax.swing.JOptionPane;
 
-import org.fpij.jitakyoei.controller.AlunoBO;
-import org.fpij.jitakyoei.controller.AlunoBOImpl;
+import org.fpij.jitakyoei.business.AlunoBO;
+import org.fpij.jitakyoei.business.AlunoBOImpl;
+import org.fpij.jitakyoei.business.EntidadeBO;
+import org.fpij.jitakyoei.business.EntidadeBOImpl;
+import org.fpij.jitakyoei.business.ProfessorBO;
+import org.fpij.jitakyoei.business.ProfessorBOImpl;
 import org.fpij.jitakyoei.model.beans.Aluno;
+import org.fpij.jitakyoei.model.beans.Entidade;
+import org.fpij.jitakyoei.model.beans.Professor;
 import org.fpij.jitakyoei.view.AppView;
 
 public class AppFacadeImpl implements AppFacade {
 	
 	private AppView view;
 	private AlunoBO alunoBO;
+	private ProfessorBO professorBO;
+	private EntidadeBO entidadeBO;
 	
 	public AppFacadeImpl(AppView view) {
 		this.view = view;
 		this.alunoBO = new AlunoBOImpl(view);
+		this.professorBO = new ProfessorBOImpl(view);
+		this.entidadeBO = new EntidadeBOImpl(view);
 	}
 
 	private void reportException(Exception e){
@@ -22,16 +32,17 @@ public class AppFacadeImpl implements AppFacade {
 		throw new RuntimeException();
 	}
 	
+	@Override
 	public void createAluno(Aluno aluno) {
 		System.out.println("AppFacadeImpl.createAluno()");
 		try{
 			alunoBO.createAluno(aluno);
 		}catch (Exception e) {
-			JOptionPane.showMessageDialog(null, e.getMessage());
 			reportException(e);
 		}
 	}
 
+	@Override
 	public void updateAluno(Aluno aluno) {
 		try{
 			alunoBO.updateAluno(aluno);
@@ -40,6 +51,7 @@ public class AppFacadeImpl implements AppFacade {
 		}
 	}
 
+	@Override
 	public void searchAluno(Aluno aluno) {
 		try{
 			alunoBO.createAluno(aluno);
@@ -48,7 +60,8 @@ public class AppFacadeImpl implements AppFacade {
 		}
 	}
 
-	public void listAlunos(Aluno aluno) {
+	@Override
+	public void listAlunos() {
 		try{
 			alunoBO.listAll();
 		}catch (Exception e) {
@@ -56,4 +69,76 @@ public class AppFacadeImpl implements AppFacade {
 		}
 	}
 
+	@Override
+	public void createProfessor(Professor professor) {
+		try{
+			professorBO.createProfessor(professor);
+		}catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+			reportException(e);
+		}		
+	}
+
+	@Override
+	public void listProfessores() {
+		try{
+			professorBO.listAll();
+		}catch (Exception e) {
+			reportException(e);
+		}
+	}
+
+	@Override
+	public void searchProfessor(Professor professor) {
+		try{
+			professorBO.searchProfessor(professor);
+		}catch (Exception e) {
+			reportException(e);
+		}
+	}
+
+	@Override
+	public void updateProfessor(Professor professor) {
+		try{
+			professorBO.updateProfessor(professor);
+		}catch (Exception e) {
+			reportException(e);
+		}
+	}
+
+	@Override
+	public void createEntidade(Entidade entidade) {
+		try{
+			entidadeBO.createEntidade(entidade);
+		}catch (Exception e) {
+			reportException(e);
+		}
+	}
+
+	@Override
+	public void listEntidade() {
+		try{
+			entidadeBO.listAll();
+		}catch (Exception e) {
+			reportException(e);
+		}
+	}
+
+	@Override
+	public void searchEntidade(Entidade entidade) {
+		try{
+			entidadeBO.searchEntidade(entidade);
+		}catch (Exception e) {
+			reportException(e);
+		}
+	}
+
+	@Override
+	public void updateEntidade(Entidade entidade) {
+		try{
+			entidadeBO.updateEntidade(entidade);
+		}catch (Exception e) {
+			reportException(e);
+		}
+	}
 }
